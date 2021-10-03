1a31ac3e4b1fc0f2298d74ab00d8d9a0d077fedd
Satish Dasyam <satishdasyam@users.noreply.github.com>
2021-10-03 16:07:11 +0530

    change project path

--- run_os_commands.py
+++ run_os_commands.py
@@ -1,13 +1,13 @@
 import subprocess
 from subprocess import check_output
 
-project_path = "C:\\Users\\satishdasyam\\AndroidStudioProjects\\VmAndroidApp\\android-app\\android-nag-client"
+project_path = "C:\\Users\\satishdasyam\\AndroidStudioProjects\\MyApplication"
 
 # check_output(["cd", project_path], shell=True).decode()
 # output = check_output("cd", shell=True).decode()
 # output1 = check_output(["cd", project_path, "gradlew.bat", "checkstyle"], shell=True).decode()
 
-# command = '''cd "C:\\Users\\satishdasyam\\AndroidStudioProjects\\VmAndroidApp\\android-app\\android-nag-client"; gradlew checkstyle'''
+# command = '''cd "C:\\Users\\satishdasyam\\AndroidStudioProjects\\MyApplication"; gradlew checkstyle'''
 # command = 'cd'
 commands = ["cd", project_path, "gradlew", "checkstyle"]
 output = subprocess.run(commands, capture_output=True, shell=True).stdout.decode()
